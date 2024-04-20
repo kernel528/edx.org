9da9cd185d28895b19a170da1fdba21752514d8d
Joe Sanders <kernel528@gmail.com>
2024-04-20 18:09:42 -0500

    Initial commit with consolidated folders for edx courses.

---
+++ factor-express-demo/README.md
@@ -0,0 +1,1 @@
+# This repo contains stuff from the Edx.org learning modules.
---
+++ factor-express-demo/falcor-app-server/index.js
@@ -0,0 +1,26 @@
+// index.js
+const falcorExpress = require("falcor-express");
+const Router = require("falcor-router");
+
+const express = require("express");
+const app = express();
+
+app.use(
+    "/model.json",
+    falcorExpress.dataSourceRoute(function (req, res) {
+        // create a Virtual JSON resource with single key ('greeting')
+        return new Router([
+            {
+                // match a request for the key 'greeting'
+                route: "greeting",
+                // respond with a PathValue with the value of 'Hello World.'
+                get: () => ({ path: ["greeting"], value: "Hello World" }),
+            },
+        ]);
+    })
+);
+
+// serve static files from current directory
+app.use(express.static(__dirname + "/"));
+
+app.listen(3000);
